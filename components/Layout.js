3041a65583357af10a515e26b5301d56cfb3d871
Axl Crawford <ackah.axl@gmail.com>
2021-07-18 13:58:11 +0100

    Fix warning

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,11 @@ const Layout = ({ children }) => {
       <Head>
         <title>Baby updates</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
           rel="stylesheet"
