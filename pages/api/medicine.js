ab6f546effccc0937611bc1282bf51b31843c88c
Axl <2830401+axcellence@users.noreply.github.com>
2021-12-31 17:49:14 +0000

    Add medicine tracking

---
+++ pages/api/medicine.js
@@ -0,0 +1,47 @@
+const moment = require("moment-timezone");
+const { Client } = require("@notionhq/client");
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export default async (req, res) => {
+  const databaseId = process.env.NOTION_DB_MEDICINE_ID;
+
+  const { amount } = req.query;
+
+  const currentTime = moment().tz("Europe/London").format();
+
+  // Options - add medication 
+  
+  // add new medication 
+    const response = await notion.pages.create({
+      parent: {
+        database_id: databaseId,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "Ezekiel had medicin",
+              },
+            },
+          ],
+        },
+        Date: {
+          date: {
+            start: currentTime,
+          },
+        },
+        Amount: {
+          text: {
+            content: amount,
+          },
+        }
+      },
+    });
+
+  res.status(200);
+};
